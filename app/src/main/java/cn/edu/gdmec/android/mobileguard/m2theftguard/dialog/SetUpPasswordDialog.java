a835a3944ce5362e9ce921b8a57eea9ce52084b7
LBWNB <862496956@qq.com>
2017-10-04 15:40:59 +0800

    完成布局

---
+++ app/src/main/java/cn/edu/gdmec/android/mobileguard/m2theftguard/dialog/SetUpPasswordDialog.java
@@ -0,0 +1,8 @@
+package cn.edu.gdmec.android.mobileguard.m2theftguard.dialog;
+
+/**
+ * Created by Administrator on 2017/10/4/004.
+ */
+
+public class SetUpPasswordDialog {
+}
---
+++ app/src/main/java/cn/edu/gdmec/android/mobileguard/m2theftguard/utils/MD5Utils.java
@@ -0,0 +1,8 @@
+package cn.edu.gdmec.android.mobileguard.m2theftguard.utils;
+
+/**
+ * Created by Administrator on 2017/10/4/004.
+ */
+
+public class MD5Utils {
+}
