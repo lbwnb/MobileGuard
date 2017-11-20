9f7a7b5a63057f625dab95994dbe2249b54490e3
HengistLee1 <392862724@qq.com>
2017-11-20 15:37:18 +0800

    m6创建包和类

---
+++ app/src/main/java/cn/edu/gdmec/android/mobileguard/m6cleancache/adapter/CacheCleanAdapter.java
@@ -0,0 +1,10 @@
+package cn.edu.gdmec.android.mobileguard.m6cleancache.adapter;
+
+import android.widget.BaseAdapter;
+
+/**
+ * Created by student on 17/11/20.
+ */
+
+public class CacheCleanAdapter {
+}
