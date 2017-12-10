da80fc92468e126f9f9d64c688602d10379937bc
qiulifeng <1170537031@qq.com>
2017-12-10 15:08:24 +0800

    完成

--- app/src/main/java/cn/edu/gdmec/android/mobileguard/m1home/HomeActivity.java
+++ app/src/main/java/cn/edu/gdmec/android/mobileguard/m1home/HomeActivity.java
@@ -26,6 +26,7 @@ import cn.edu.gdmec.android.mobileguard.m4appmanager.AppManagerActivity;
 import cn.edu.gdmec.android.mobileguard.m5virusscan.VirusScanActivity;
 import cn.edu.gdmec.android.mobileguard.m6cleancache.CacheClearListActivity;
 import cn.edu.gdmec.android.mobileguard.m8trafficmonitor.TrafficMonitoringActivity;
+import cn.edu.gdmec.android.mobileguard.sm9advancedtool.AdvancedToolsActivity;
 
 public class HomeActivity extends AppCompatActivity {
 private GridView gv_home;
@@ -72,6 +73,9 @@ private GridView gv_home;
                         break;
                     case 6:
                         startActivity(TrafficMonitoringActivity.class);
+                        break;
+                    case 7:
+                        startActivity(AdvancedToolsActivity.class);
                         break;
 
                 }
